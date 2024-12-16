26f205381db5bd5addd8658cadc29261f080f83a
Emirhan Soylu <61618968+EmirhanSyl@users.noreply.github.com>
2024-12-16 22:17:11 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# HireWise
+ 
---
+++ src/main/java/com/talentbridge/hirewise/Main.java
@@ -0,0 +1,11 @@
+package com.talentbridge.hirewise;
+
+/**
+ *
+ * @author talentbridge
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Helllllllooooooo");
+    }
+}
