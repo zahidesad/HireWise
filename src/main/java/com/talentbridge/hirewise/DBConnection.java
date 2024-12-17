f04fca019db79b607ff55878d28b8f2de5589d07
Zahid Esad <116666407+zahidesad@users.noreply.github.com>
2024-12-17 20:17:51 +0300

    Connection information was hidden
    Changed the method of keeping connection information. Connection information such as DB_URL, DB_USER and DB_PASSWORD kept in system enviorenment

--- src/main/java/com/talentbridge/hirewise/DBConnection.java
+++ src/main/java/com/talentbridge/hirewise/DBConnection.java
@@ -1,32 +1,23 @@
 package com.talentbridge.hirewise;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 /**
  *
  * @author zahid
  */
 public class DBConnection {
-    
-    private static String URL;
-    private static String USER;
-    private static String PASSWORD;
+
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     static {
-        try (InputStream input = DBConnection.class.getResourceAsStream("/config.properties")) {
-            Properties prop = new Properties();
-            prop.load(input);
-            URL = prop.getProperty("DB_URL");
-            USER = prop.getProperty("DB_USER");
-            PASSWORD = prop.getProperty("DB_PASSWORD");
-
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (IOException | ClassNotFoundException ex) {
+        } catch (ClassNotFoundException ex) {
             ex.printStackTrace();
         }
     }
@@ -34,5 +25,5 @@ public class DBConnection {
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
-    
+
 }
